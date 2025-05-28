aa0e7ad4a66db250e2bb233df7288f40e2bfa6c6
Anna Gubina <anya.gubina101@gmail.com>
2025-05-28 12:20:15 -0400

    Третья доработка

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,6 +1,7 @@
 
 const xhr = new XMLHttpRequest();
-const form = document.querySelector('.signin')
+const signIn = document.querySelector('.signin')
+const form = document.querySelector('.signin__form')
 const btn = document.querySelector('#signin__btn')
 const welcome = document.querySelector('.welcome')
 const userId = document.querySelector('#user_id')
@@ -10,24 +11,25 @@ function formSend() {
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     xhr.responseType = 'json';
 
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
-            if(xhr.response.success) {
-                form.classList.remove('signin_active')
-                localStorage.user = xhr.response.user_id;
-                welcome.classList.add('welcome_active');
-                userId.textContent = xhr.response.user_id;
-            } else {
-                alert('«Неверный логин/пароль»')
-            }
-
-            if(localStorage.getItem('user')) {
-                welcome.classList.add('welcome_active');
-                 userId.textContent = localStorage.getItem('user')
-            }
+    xhr.onload = () => {
+        if(xhr.response.success) {
+            signIn.classList.remove('signin_active')
+            localStorage.user = xhr.response.user_id;
+            welcome.classList.add('welcome_active');
+            userId.textContent = xhr.response.user_id;
+        } else {
+            alert('«Неверный логин/пароль»')
         }
-    })
+    }
+
+    if(localStorage.getItem('user')) {
+        welcome.classList.add('welcome_active');
+        userId.textContent = localStorage.getItem('user')
+    } else {
+        signIn.classList.add('signin_active');
+    }
     
+    form.reset();
     xhr.send(formData);
 }
 
